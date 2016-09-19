584c95b19342762aede303e23520bf4db0e793fa
Sami Barghshoon <chugah@users.noreply.github.com>
2016-09-19 15:13:26 -0400

    Initial commit

---
+++ app/api/TodoAPI.jsx
@@ -0,0 +1,49 @@
+var $ = require('jquery');
+
+module.exports = {
+	setTodos: function (todos) {
+		if ($.isArray(todos)) {
+			localStorage.setItem('todos', JSON.stringify(todos));
+			return todos;
+		}
+	},
+
+	getTodos: function () {
+		var stringTodos = localStorage.getItem('todos');
+		var todos = [];
+
+		try {
+			todos = JSON.parse(stringTodos);
+		} catch (e) {
+
+		}
+
+		return $.isArray(todos) ? todos : [];
+	},
+
+	filterTodos: function (todos, showCompleted, searchText) {
+		var filteredTodos = todos;
+
+		//Filter by showCompleted
+		filteredTodos = filteredTodos.filter( (todo) => {
+			return !todo.completed || showCompleted;
+		});
+
+		//Filter by searchText
+		var filteredTodos = filteredTodos.filter( (todo) => {
+			return searchText === '' || todo.text.toLowerCase().indexOf(searchText) > -1;
+		});
+
+		//Sort todos with non-completed first
+		filteredTodos.sort( (a, b) => {
+			if (!a.completed && b.completed) {
+				return -1;
+			} else if (a.completed && !b.completed) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+		return filteredTodos;
+	}
+};
