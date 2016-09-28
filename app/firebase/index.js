dbc42283bf47038635553df28b07993830c3f131
Sami Barghshoon <chugah@users.noreply.github.com>
2016-09-28 17:27:38 -0400

    Add actions for firebase login and logout

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,17 +2,19 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyCQ_feH-LmhIOHsche0aITZd0nMSmIStqY",
-    authDomain: "rocvoc-todo-app.firebaseapp.com",
-    databaseURL: "https://rocvoc-todo-app.firebaseio.com",
-    storageBucket: "rocvoc-todo-app.appspot.com",
-    messagingSenderId: "942985709338"
+    apiKey: "AIzaSyC6CTLWrPho1JsueCn8RYaHzmRTfEdeeMs",
+    authDomain: "todoapptest-7f7d3.firebaseapp.com",
+    databaseURL: "https://todoapptest-7f7d3.firebaseio.com",
+    storageBucket: "todoapptest-7f7d3.appspot.com",
+    messagingSenderId: "5920321842"
   };
 
   firebase.initializeApp(config);
 } catch (e) {
 
 }
+
+export var githubProvider = new firebase.auth.GithubAuthProvider();
 
 export var firebaseRef = firebase.database().ref();
 export default firebase;
